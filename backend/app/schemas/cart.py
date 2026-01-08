fbc3f8197c7fd7ac1b27d32f0ffcb427c4d244b4
namvste <144486361+CtrlAltDe1ete@users.noreply.github.com>
2026-01-08 19:40:03 +0200

    add routes

---
+++ backend/app/schemas/cart.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+
+class CartItemBase(BaseModel):
+    product_id: int = Field(..., description="Product ID")
+    quantity: int = Field(..., description="Quantity", gt=0)
+    
+    
+class CartItemCreate(CartItemBase):
+    pass
+
+class CartItemUpdate(BaseModel):
+    product_id: int = Field(..., description="Product ID")
+    quantity: int = Field(..., qt=0, description="Quantity")
+    
+class CartItem(BaseModel):
+    product_id: int
+    name: str = Field(..., description="Product name")
+    price: float = Field(..., description="Product price")
+    quantity: int = Field(..., description="Product quantity")
+    subtotal: float = Field(..., description="Total price ")
+    image_url: Optional[str] =Field(None, description="Product image URL")
+    
+class CartResponse(BaseModel):
+    items: list[CartItem] = Field(..., description="List of items in cart")
+    total: float = Field(..., description="Total cart price")
+    items_count: int = Field(..., description="Total number of items in cart")
+    
